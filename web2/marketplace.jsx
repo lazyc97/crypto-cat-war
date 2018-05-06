b440e85f69d9b90e845ca5482bc4dcf9d1e23036
lazyc97 <lazycoder97@gmail.com>
2018-05-06 21:38:52 +0700

    breeding

--- web2/marketplace.jsx
+++ web2/marketplace.jsx
@@ -185,10 +185,146 @@ class AuctionBlock extends React.Component {
   }
 };
 
+class BreedingBlock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.DEFAULT_INFO = {
+      atk: 0,
+      def: 0,
+      hp: 0,
+      atkPerLv: 0,
+      defPerLv: 0,
+      hpPerLv: 0,
+      level: 0,
+      element: -1,
+      breedFee: Ethers.utils.bigNumberify('0'),
+    };
+
+    this.ownCatInputBox = React.createRef();
+    this.breedCatInputBox = React.createRef();
+
+    this.getInfo1 = async () => {
+      try {
+        const id = Ethers.utils.bigNumberify(this.ownCatInputBox.current.value);
+        const info = await getFullCatData(id);
+        if (!info.isMale) throw 'Cat should be MALE';
+        if (info.owner !== MainContract.signer.address) throw 'This should be your cat';
+        this.setState({
+          info1: info,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    this.getInfo2 = async () => {
+      try {
+        const id = Ethers.utils.bigNumberify(this.breedCatInputBox.current.value);
+        const info = await getFullCatData(id);
+        if (info.isMale) throw 'Cat should be FEMALE';
+        this.setState({
+          info2: info,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    this.breed = async () => {
+      const { info1, info2 } = this.state;
+      if (info1.level > 0 && info2.level > 0) {
+        try {
+          const value = info2.owner === MainContract.signer.address ? 0 : info2.breedFee;
+          await MainContract.payForBreed(info1.id, info2.id, { value: value });
+          alert('New Kitty has been born!');
+        } catch (err) {
+          console.error(err);
+        }
+      } else {
+        alert('Please select cats first.');
+      }
+    };
+
+    this.state = {
+      info1: this.DEFAULT_INFO,
+      info2: this.DEFAULT_INFO,
+    };
+  }
+
+  render() {
+    const { info1, info2 } = this.state;
+
+    return (
+      <section className="fdb-block">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-md-6 m-md-auto ml-lg-0 col-lg-5">
+              <span>
+                <p>Owner: <strong>{info1.owner}</strong></p>
+                <p>Element: <strong>{CAT_ELEMENTS[info1.element]}</strong></p>
+                <p>Level: <strong>{info1.level}</strong></p>
+                <p>Atk: <strong>{info1.atk}</strong> - Lv Up: <strong>+{info1.atkPerLv}</strong></p>
+                <p>Def: <strong>{info1.def}</strong> - Lv Up: <strong>+{info1.defPerLv}</strong></p>
+                <p>Hp: <strong>{info1.hp}</strong> - Lv Up: <strong>+{info1.hpPerLv}</strong></p>
+              </span>
+              <p className="display-4">With</p>
+              <span>
+                <p>Owner: <strong>{info2.owner}</strong></p>
+                <p>Level: <strong>{info2.level}</strong></p>
+                {info2.owner !== MainContract.signer.address ? (
+                  <p>Breed Fee: <strong>{Ethers.utils.formatEther(info2.breedFee)} ETH</strong></p>
+                ) : (
+                  <p>Breed Fee: <strong>Free</strong></p>
+                )}
+              </span>
+            </div>
+            <div className="col-12 col-md-10 col-lg-6 mt-4 mt-lg-0 ml-auto mr-auto ml-lg-auto text-left">
+              <div className="row">
+                <div className="col">
+                  <h1>Breeding</h1>
+                  <p className="text-h3">Input the ID of your male cat and the female cat you want it to breed with.</p>
+                </div>
+              </div>
+              <div className="row mt-2">
+                <div className="col">
+                  <div className="input-group">
+                    <input ref={this.ownCatInputBox} type="text" className="form-control" placeholder="Your cat's ID"></input>
+                    <span className="input-group-btn">
+                      <button className="btn" type="button" onClick={this.getInfo1}>Select</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-2">
+                <div className="col">
+                  <div className="input-group">
+                    <input ref={this.breedCatInputBox} type="text" className="form-control" placeholder="Breed cat's ID"></input>
+                    <span className="input-group-btn">
+                      <button className="btn" type="button" onClick={this.getInfo2}>Select</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="row mt-4">
+                <div className="col">
+                  <span className="input-group-btn">
+                    <button className="btn" type="button" onClick={this.breed}>Breed</button>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
 const Marketplace = () => (
   <React.Fragment>
     <InitCatBlock />
     <AuctionBlock />
+    <BreedingBlock />
   </React.Fragment>
 );
 
